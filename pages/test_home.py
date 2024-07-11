58bb4d16d87aef37e7a9af17e18c1deeab50c776
arpit <arpit@idreameducation.org>
2024-07-11 22:03:02 +0530

    Structured

---
+++ pages/test_home.py
@@ -0,0 +1,7 @@
+
+
+driver.find_element(AppiumBy.ID, lp._yesPopupButton).click()
+            driver.find_element(AppiumBy.ID, lp._whileUsingTheApp).click()
+            driver.find_element(AppiumBy.ID, lp._hamburgerIcon).click()
+            driver.find_element(AppiumBy.ID, lp._signOut).click()
+            driver.find_element(AppiumBy.ID, lp._yesPopupButton).click()
